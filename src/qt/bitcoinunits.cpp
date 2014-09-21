8843991e1eb02724d1f07549434293601121fbf7
przyjazny_puchacz1 <paweleg@ubuntu.(none)>
2014-09-21 04:09:33 -0700

    zmienione na concretecoin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("CORE");
+    case mBTC: return QString("mCORE");
+    case uBTC: return QString::fromUtf8("μCORE");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("ConcreteCoin");
+    case mBTC: return QString("Milli-ConcreteCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-ConcreteCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
